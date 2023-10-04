282c7bf2466efda22be369aef8bd655cd0e579b5
tinyphoniex <royalguy1031@gmail.com>
2023-10-04 21:38:43 +0900

    add User Search in Tools page

--- src/app/(app)/admin/tools/page.tsx
+++ src/app/(app)/admin/tools/page.tsx
@@ -1,36 +1,79 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import clsx from "clsx";
 
-import { Tabs, TabsProps } from 'antd';
+import UserSearch from "./UserSearch";
 
-const TabPanes: TabsProps['items'] = [
-    {
-        label: "Search User",
-        key: '1',
-        children: `Content of Tab Search Users`,
-    },
-    {
-        label: "Coupons",
-        key: '2',
-        children: `Content of Tab Coupons`,
-    },
-    {
-        label: "Search Coupon",
-        key: '3',
-        children: `Content of Tab  Search Coupon`,
-    }
-]
+const Tools = () => {
+  const [currentTab, setCurrentTab] = useState("Users");
 
-function Tools() {
   return (
-    <div>
-       <Tabs
-            className='text-[#ddd]'
-            defaultActiveKey="1"
-            centered
-            items={TabPanes}
-            /> 
-    </div>
-  )
-}
+    <section className="flex flex-col w-full overflow-y-auto h-[calc(100vh-60px)]">
+      <p className="text-lg text-white bg-brand-title p-4">Reports - {currentTab}</p>
+      <section className="text-sm font-medium text-center text-white bg-brand-title">
+        <ul className="flex flex-wrap -mb-px">
+          <li className="mr-2">
+            <a
+              className={clsx(
+                "inline-block px-4 cursor-pointer pb-2",
+                currentTab === "Users" ? "border-white border-b-4" : ""
+              )}
+              onClick={() => setCurrentTab("Users")}
+            >
+              User Search
+            </a>
+          </li>
+          <li className="mr-2">
+            <a
+              className={clsx(
+                "inline-block px-4 cursor-pointer pb-2",
+                currentTab === "Coupons"
+                  ? "border-white border-b-4"
+                  : ""
+              )}
+              onClick={() => setCurrentTab("Coupons")}
+            >
+              Coupons
+            </a>
+          </li>
+          <li className="mr-2">
+            <a
+              className={clsx(
+                "inline-block px-4 cursor-pointer pb-2",
+                currentTab === "Search Coupon" ? "border-white border-b-4" : ""
+              )}
+              onClick={() => setCurrentTab("Search Coupon")}
+            >
+              Search Coupon
+            </a>
+          </li>
+          <li className="mr-2">
+            <a
+              className={clsx(
+                "inline-block px-4 cursor-pointer pb-2",
+                currentTab === "Slot Transactions" ? "border-white border-b-4" : ""
+              )}
+              onClick={() => setCurrentTab("Slot Transactions")}
+            >
+              Slot Transactions
+            </a>
+          </li>
+          <li className="mr-2">
+            <a
+              className={clsx(
+                "inline-block px-4 cursor-pointer pb-2",
+                currentTab === "Casino Transactions" ? "border-white border-b-4" : ""
+              )}
+              onClick={() => setCurrentTab("Casino Transactions")}
+            >
+              Casino Transactions
+            </a>
+          </li>
+        </ul>
+      </section>
+      <UserSearch currentTab={currentTab} />
+    </section>
+  );
+};
 
-export default Tools; 
+export default Tools;

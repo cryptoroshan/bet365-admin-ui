36687dfd68d8e67b6ce5c12f20a3c2fb45be4069
tinyphoniex <royalguy1031@gmail.com>
2023-10-13 19:38:12 +0900

    add Rakes UI

--- src/app/(app)/admin/rakes/page.tsx
+++ src/app/(app)/admin/rakes/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import requireSession from "@/lib/request-session";
+import RakesContent from "./RakesContent";
 
-function Rakes() {
+const RakesPage = async ({ children }: { children: React.ReactNode }) => {
+  await requireSession();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex flex-col w-full overflow-y-auto h-[calc(100vh-60px)]">
+      <p className="text-lg text-white bg-brand-title p-4">Rakes</p>
+      <RakesContent />
+    </section>
+  );
+};
 
-export default Rakes;
+export default RakesPage;
